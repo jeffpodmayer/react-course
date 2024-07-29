38f38a299086b58e7a57dd07888b2f680b36ccad
Jeffrey Podmayer <jeff.podmayer@gmail.com>
2024-07-29 15:42:06 -0700

    built reusable button

--- steps/src/App.js
+++ steps/src/App.js
@@ -10,6 +10,15 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <StepMessage step={1}>
+        <Button
+          bgColor="#e7e7e7"
+          textColor="#333"
+          onClick={() => alert("Learn how to")}
+        >
+          Learn How
+        </Button>
+      </StepMessage>
       {/* <Steps /> */}
     </div>
   );
@@ -39,11 +48,7 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
               <span>👈 </span>Previous
@@ -54,6 +59,15 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3> Step {step}</h3>
+      {children}
     </div>
   );
 }
