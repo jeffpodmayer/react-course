73015f77b226b54bb369fecc886d33d10421c03f
Jeffrey Podmayer <jeff.podmayer@gmail.com>
2024-07-30 14:40:16 -0700

    built form and formBill components

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FromSplitBill />
     </div>
   );
 }
@@ -56,7 +59,49 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You are even with your friends.</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👯‍♀️ Friend</label>
+      <input type="text" />
+
+      <label>🌅 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FromSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💰 Bill Value</label>
+      <input type="text" />
+
+      <label>😇 Your Expense</label>
+      <input type="text" />
+
+      <label>💥 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   );
 }
